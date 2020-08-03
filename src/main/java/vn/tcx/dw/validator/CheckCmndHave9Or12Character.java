4b8815189929e42d3f6a1b55822e4afc21da5096
hieuvv <vanhieuit97@gmail.com>
2020-08-03 18:02:56 +0700

    update code check cmnd va check all la chu

---
+++ src/main/java/vn/tcx/dw/validator/CheckCmndHave9Or12Character.java
@@ -0,0 +1,42 @@
+package vn.tcx.dw.validator;
+
+import java.util.Objects;
+
+import vn.tcx.dw.component.Result;
+import vn.tcx.dw.component.Validator;
+
+/**
+ * Check number cmnd 9 or 12 character
+ * 
+ * @author hieuvv
+ * @since 1.0
+ * @created 03/08/2020 18:01:30
+ */
+public class CheckCmndHave9Or12Character implements Validator {
+
+    /**
+     * Perform checj Cmnd have 9 or 12 character
+     * 
+     * @update hieuvv
+     * @lastModifier 03/08/2020 18:00:29
+     * @param value
+     * @return
+     */
+    private boolean isValidate(String value) {
+
+        int length = value.length();
+
+        return length == 9 || length == 12;
+    }
+
+    @Override
+    public Result validate(Object value) {
+
+        if (Objects.isNull(value)) {
+            return Result.OK;
+        }
+
+        return isValidate(value.toString()) ? Result.OK : Result.FAILED;
+    }
+
+}
---
+++ src/main/java/vn/tcx/dw/validator/CheckStringIsAllLetterValidator.java
@@ -0,0 +1,32 @@
+package vn.tcx.dw.validator;
+
+import java.util.Objects;
+
+import org.apache.commons.lang3.StringUtils;
+
+import vn.tcx.dw.component.Result;
+import vn.tcx.dw.component.Validator;
+
+/**
+ * Check String is all letter
+ * 
+ * @author hieuvv
+ * @since 1.0
+ * @created 27/07/2020 10:07:28
+ */
+public class CheckStringIsAllLetterValidator implements Validator {
+
+    private boolean isAllLetterValidate(String value) {
+        return StringUtils.isAlpha(value);
+    }
+
+    @Override
+    public Result validate(Object value) {
+
+        if (Objects.isNull(value)) {
+            return Result.OK;
+        }
+
+        return isAllLetterValidate(value.toString()) ? Result.OK : Result.FAILED;
+    }
+}
